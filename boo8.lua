8a2911df9461eb9619c69747b7720694d797c6ad
Natalie Nguyen <taleo.sch@gmail.com>
2025-05-27 18:56:11 -0400

    Update boo8.lua
    Remove legacy header metadata

--- boo8.lua
+++ boo8.lua
@@ -1,11 +1,3 @@
---- STEAMODDED HEADER
---- MOD_NAME: Boo8
---- MOD_ID: boo8
---- MOD_AUTHOR: [:3]
---- MOD_DESCRIPTION: Changes 8 ball to 1 in 2
-
-----------------------------------------------
-------------MOD CODE -------------------------
 SMODS.Joker:take_ownership('8_ball', {
     -- properties to change
     text = {"{C:green}#1# in #2#{} chance for each",
@@ -15,5 +7,3 @@ SMODS.Joker:take_ownership('8_ball', {
     },
     config = {extra=2}              
 })
-----------------------------------------------
-------------MOD CODE END----------------------
